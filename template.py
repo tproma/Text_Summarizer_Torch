0e9d184fe462cda3272403c12f03c30aa80f2fdb
Tanjina <tanjinaproma@gmail.com>
2023-06-01 10:22:12 +0600

    template added

--- template.py
+++ template.py
@@ -2,4 +2,17 @@ import os
 from pathlib import Path
 import logging 
 
-logging.basicConfig(level = logging.INFO, format)
+logging.basicConfig(level = logging.INFO, format = '[%(asctime)s]: %(message)s:')
+
+project_name = "textSummarizer"
+
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/utils/common.py",
+    f"src/{project_name}/loggingconfig/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    
+]
