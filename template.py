d954a0745caa15d8676739d76dd1b68dc3d166fa
Tanjina <tanjinaproma@gmail.com>
2023-06-01 10:26:23 +0600

    template  updated

--- template.py
+++ template.py
@@ -12,7 +12,18 @@ list_of_files = [
     f"src/{project_name}/components/__init__.py",
     f"src/{project_name}/utils/__init__.py",
     f"src/{project_name}/utils/common.py",
-    f"src/{project_name}/loggingconfig/__init__.py",
-    f"src/{project_name}/components/__init__.py",
-    
+    f"src/{project_name}/logging//__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "params.yaml",
+    "app.py",
+    "main.py",
+    "Dockerfile",
+    "requirements.txt",
+    "setup.py"
+
 ]
