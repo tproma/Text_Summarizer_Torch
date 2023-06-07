0a85377e6b86d0e7fe0b68097ad4101b7ef3abb6
Tanjina Proma <tanjinaproma@gmail.com>
2023-06-07 11:11:03 +0600

    Update logger.py

--- logger.py
+++ logger.py
@@ -6,3 +6,13 @@ log_dir = "logs"
 log_filepath = os.path.join(log_dir, "running_logs.log")
 os.makedirs(log_dir, exist_ok = True)
 
+logging.basicConfig(
+    level = logging.INFO,
+    format = logging_str,
+    
+    handlers = [
+      logging.FileHandler(log_filepath),
+      logging.StreamHanmdler(sys.stdout)
+    ]
+)
+logger = logging.getlogger("textSummarizer")
