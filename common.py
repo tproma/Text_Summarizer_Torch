72697e0c889df3127a0ea302c69ee66c25bccd2d
Tanjina Proma <tanjinaproma@gmail.com>
2023-06-08 12:50:05 +0600

    Update common.py

--- common.py
+++ common.py
@@ -6,3 +6,12 @@ from ensure import ensure_annotations
 from box import ConfigBox
 from pathlib import Path
 from typing import Any
+
+
+def read_yaml(path_to_yaml : Path) -> ConfigBox:
+    try:
+        with open(path_to_yaml) as yaml_file:
+            content = yaml.safe_load(yaml_file)
+            logger.info(f"yaml file:{path_to_yaml} loaded successfully")
+            return ConfigBox(content)
+          
