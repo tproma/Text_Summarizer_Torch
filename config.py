a73347270c2ceb8faa19a18344ff98b02b1005cb
Tanjina Proma <tanjinaproma@gmail.com>
2023-06-10 10:30:30 +0600

    Update config.py

--- config.py
+++ config.py
@@ -8,3 +8,8 @@ class ConfigurationManager:
       self.params = read_yaml(params_filepath)
       
       create_directories([self.config.artifacts_root])
+
+    def get_data_ingestion_config(self) -> DataIngestionConfig:
+       config= self.config.data_ingestion
+       create_directories([config.root_dir])
+       
