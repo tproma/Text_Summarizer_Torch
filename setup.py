7dfcef0b3108e66f5c27cbd476aa18a55808262c
Tanjina <tanjinaproma@gmail.com>
2023-06-12 19:50:55 +0600

    modified setup

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ with open("README.md", "r", encoding="utf-8") as f:
   
 __version__ = "0.0.0"
 
-REPO_NAME = "Text_Summarizer"
-AUTHOR_USER_NAME = "Proma"
+REPO_NAME = "Text_Summarizer_Torch"
+AUTHOR_USER_NAME = "tproma"
 SRC_REPO = "textSummarizer"
 AUTHOR_EMAIL = "tanjinaproma@gmail.com"
 
@@ -20,8 +20,12 @@ setuptools.setup(
     deescription = "Pyhton package for NLP app",
     long_description = long_description,
     long_description_content = "text/markdown",
-    
+    url=f"https://github.com/tproma/Text_Summarizer_Torch.git"
+    project_urls = {
+      "Bug Tracker" : f"https://github.com/tproma/Text_Summarizer_Torch.git/issues",
+    },
   
     package_dir = {"": "src"},
+    packages = setuptools.find_packeages(where = "src")
   
 )
