0095cf78a051432f36720147429983fc89ffc53e
Tanjina <tanjinaproma@gmail.com>
2023-06-12 12:49:07 +0600

    Config

--- src/textSummarizer/config/configuration.py
+++ src/textSummarizer/config/configuration.py
@@ -16,14 +16,13 @@ class ConfigurationManager:
       create_directories([self.config.artifacts_root])
 
     def get_data_ingestion_config(self) -> DataIngestionConfig:
-       config= self.config.data_ingestion
-       create_directories([config.root_dir])
+      config= self.config.data_ingestion
+      create_directories([config.root_dir])
        
-       data_ingestion_config = DataIngestionConfig(
-           root_dir = config.root_dir,
-           source_URL = config.source_URL,
-           local_data_file = config.local_data_file,
-           unzip_dir = config.unzip_dir
+      data_ingestion_config = DataIngestionConfig(
+         root_dir = config.root_dir,
+         source_URL = config.source_URL,
+         local_data_file = config.local_data_file,
+         unzip_dir = config.unzip_dir
        )
-        
-        return data_igestion_config
+      return data_igestion_config
