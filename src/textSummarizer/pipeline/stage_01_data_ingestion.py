7d35c63624c3c13f2cf286d57d3973ed29bf80d3
Tanjina Proma <tanjinaproma@gmail.com>
2023-06-18 21:12:49 +0600

    Update stage_01_data_ingestion.py

--- src/textSummarizer/pipeline/stage_01_data_ingestion.py
+++ src/textSummarizer/pipeline/stage_01_data_ingestion.py
@@ -6,4 +6,12 @@ class DataIngestionTrainingPipeline:
   def __init_(self):
     pass
 
+  def main(self):
+    config = ConfigurationManager()
+    data_ingestion_config = config.get_data_ingestion_config()
+    data_ingestion = DataIngestion(config = data_ingestion_config)
+    data_ingestion.download_file()
+    data_ingestion.extract_zip_file()
+    
+    
   
