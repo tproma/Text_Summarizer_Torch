46dd3b0a50566bec262abf65ebd7f4f4ec6fd5cd
Tanjina Proma <tanjinaproma@gmail.com>
2023-06-18 21:10:14 +0600

    Create stage_01_data_ingestion.py

---
+++ src/textSummarizer/pipeline/stage_01_data_ingestion.py
@@ -0,0 +1,9 @@
+from textSummarizer.config.configuration import ConfigurationManager
+from textSummarizer.components.data_ingestion import DataIngestion
+from textSummarizer.logging import logger
+
+class DataIngestionTrainingPipeline:
+  def __init_(self):
+    pass
+
+  
