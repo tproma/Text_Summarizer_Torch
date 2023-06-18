a92ede8646ee9769b9f6f6007664a0b58b376f33
Tanjina Proma <tanjinaproma@gmail.com>
2023-06-18 21:17:31 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 from textSummarizer.logging import logger
+from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("welcome to the App")
+
+STAGE_NAME = "Data Ingestion stage"
+
+try:
+  logger.info(f" >>>>>>> stage  {STAGE_NAME} started <<<<<<<<")
