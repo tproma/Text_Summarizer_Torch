0bae04a5f61a365ee40d8664688f8ba858804c3b
Tanjina Proma <tanjinaproma@gmail.com>
2023-06-19 09:36:18 +0600

    Update main.py

--- main.py
+++ main.py
@@ -6,3 +6,10 @@ STAGE_NAME = "Data Ingestion stage"
 
 try:
   logger.info(f" >>>>>>> stage  {STAGE_NAME} started <<<<<<<<")
+  data_ingestion = DataIngestionTrainingPipeline()
+  data_ingestion.main()
+  logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<<\n\nx========x")
+  
+except Exception as e:
+  logger.exception(e)
+  raise e
