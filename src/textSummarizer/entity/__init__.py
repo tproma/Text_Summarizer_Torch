43b09119f6669a2cd1e9b3e191a11cb57af294f8
Tanjina Proma <tanjinaproma@gmail.com>
2023-06-20 11:35:01 +0600

    data ingestion config updated

---
+++ src/textSummarizer/entity/__init__.py
@@ -0,0 +1,9 @@
+from dataclass import dataclass
+from pathlib import Path
+
+@dataclass(frozen=True)
+class DataIngestionConfig:
+    root_dir: Path
+    source_URL: str
+    local_data_file: Path
+    unzip_dir: Path
