880896dbbd11972020d86924b0249a532f60b68e
Tanjina Proma <tanjinaproma@gmail.com>
2023-06-21 10:48:35 +0600

    Create data_validation.py

---
+++ src/textSummarizer/components/data_validation.py
@@ -0,0 +1,10 @@
+import os
+from textSummarizer.logging import logger
+from textSummarizer.entity import DataValidationConfig
+
+class DataValidation:
+  def __init__(self, config: DataValidationConfig):
+    self.config = config
+
+
+  
