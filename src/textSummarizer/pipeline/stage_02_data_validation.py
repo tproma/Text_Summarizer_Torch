4d97f223ded14fc02777bd623c81c4c4685b69d2
Tanjina Proma <tanjinaproma@gmail.com>
2023-06-22 17:33:03 +0600

    Update stage_02_data_validation.py

--- src/textSummarizer/pipeline/stage_02_data_validation.py
+++ src/textSummarizer/pipeline/stage_02_data_validation.py
@@ -1,1 +1,15 @@
 
+from textSummarizer.config.configuration import ConfigurationManager
+from textSummarizer.components.data_ingestion import Validation
+from textSummarizer.logging import logger
+
+class DataValidationTrainingPipeline:
+  def __init_(self):
+    pass
+
+  def main(self):
+    config = ConfigurationManager()
+    data_validation_config = config.get_data_validation_config()
+    data_validation = DataIngestion(config = data_validation_config)
+    data_validation.download_file()
+    data_validation.extract_zip_file()
