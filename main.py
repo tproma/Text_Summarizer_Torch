6a2fde807a1035534bcd5300a52a60e9a7b84df3
Tanjina Proma <tanjinaproma@gmail.com>
2023-06-22 17:36:11 +0600

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,21 @@ try:
   data_ingestion = DataIngestionTrainingPipeline()
   data_ingestion.main()
   logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<<\n\nx========x")
+except Exception as e:
+  logger.exception(e)
+  raise e
+
+
+
+STAGE_NAME = "Data validation stage"
+try:
+  logger.info(f" >>>>>>> stage  {STAGE_NAME} started <<<<<<<<")
+  data_validation = DataValidationTrainingPipeline()
+  data_validation.main()
+  logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<<\n\nx========x")
   
 except Exception as e:
   logger.exception(e)
   raise e
+
+
