87593db11c001702569945f18f87fe298ea4466a
Tanjina <tanjinaproma@gmail.com>
2023-06-23 15:35:43 +0600

    validation notebook deleted

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainin
 
 
 STAGE_NAME = "Data Ingestion stage"
-
 try:
   logger.info(f" >>>>>>> stage  {STAGE_NAME} started <<<<<<<<")
   data_ingestion = DataIngestionTrainingPipeline()
@@ -21,7 +20,6 @@ try:
   data_validation = DataValidationTrainingPipeline()
   data_validation.main()
   logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<<\n\nx========x")
-  
 except Exception as e:
   logger.exception(e)
   raise e
