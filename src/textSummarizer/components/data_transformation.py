cb4e1ab788aeac8c4c29a56bba1274110d8cc447
Tanjina <tanjinaproma@gmail.com>
2023-06-29 01:40:24 +0600

    transformation pipeline  created

--- src/textSummarizer/components/data_transformation.py
+++ src/textSummarizer/components/data_transformation.py
@@ -2,7 +2,7 @@ import os
 from textSummarizer.logging import logger
 from transformers import AutoTokenizer
 from datasets import load_dataset, load_from_disk
-from textSummarizer.entity import Da
+from textSummarizer.entity import DataTransformationConfig
 
 
 class DataTransformation:
